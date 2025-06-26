c6f7bd34147724065db187d9cdac79ad991d7256
Edward Kong <edwardkong.kong@maybank.com>
2025-06-26 22:38:59 +0800

    fix stylesheets

--- packages/auth/src/screens/SignInScreen.tsx
+++ packages/auth/src/screens/SignInScreen.tsx
@@ -6,6 +6,28 @@ import {useAuth} from '../contexts/AuthContext';
 const SignInScreen = () => {
   const {signIn} = useAuth();
   const { colors } = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    welcomeHeadline: {
+      color: colors.primary20,
+    },
+    welcomeText: {
+      padding: 16,
+      paddingBottom: 32,
+    },
+    button: {
+      backgroundColor: colors.primary90,
+      padding: 16,
+      borderRadius: 16,
+    },
+  });
+  
   return (
     <View style={styles.container}>
       <Text variant="headlineMedium" style={styles.welcomeHeadline}>
@@ -22,25 +44,6 @@ const SignInScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcomeHeadline: {
-    color: colors.primary20,
-  },
-  welcomeText: {
-    padding: 16,
-    paddingBottom: 32,
-  },
-  button: {
-    backgroundColor: colors.primary90,
-    padding: 16,
-    borderRadius: 16,
-  },
-});
+
 
 export default SignInScreen;
